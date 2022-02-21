63e393f0acd644687a6ab761d67d8de590c3279f
CTH <cth@email.com>
2022-02-21 20:58:08 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,9 +1,11 @@
 package com.atguigu;
 
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git3!");
+        System.out.println("hello git4!");
     }
 }
