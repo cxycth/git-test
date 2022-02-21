ea2b1118c38eae8d35f46ee9c746a93d0890a498
CTH <cth@email.com>
2022-02-21 21:19:47 +0800

    first commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -1,12 +1,7 @@
 package com.atguigu;
 
-
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello git2!");
-        System.out.println("hello git3!");
-        System.out.println("hello git4!");
-        System.out.println("master test!");
+        System.out.println("hello git1!");
     }
 }
